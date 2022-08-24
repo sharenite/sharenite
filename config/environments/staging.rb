48d2b2d60efa83d45a25e5b0745f3bd38ac62fd2
Dawid Karczewski <dawidxen@gmail.com>
2022-08-24 14:40:06 +0200

    Adds staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Just use the production settings
+require File.expand_path('production.rb', __dir__)
+
+Rails.application.configure do
+  # Here override any defaults
+end
