ad6dff1d13b21c0b5e2fa89dd2d5637d3b3adff7
Dawid Karczewski <dawidxen@gmail.com>
2022-08-27 23:06:51 +0200

    Adds params to games api

--- app/controllers/api/v1/games.rb
+++ app/controllers/api/v1/games.rb
@@ -15,6 +15,38 @@ module API
         params do
           requires :games, type: Array do
             requires :name, type: String
+            optional :added, type: DateTime
+            optional :community_score, type: Integer
+            optional :critic_score, type: Integer
+            optional :description, type: String
+            optional :favorite, type: Boolean
+            optional :game_id, type: String
+            optional :game_started_script, type: String
+            optional :hidden, type: Boolean
+            optional :include_library_plugin_action, type: Boolean
+            optional :install_directory, type: String
+            optional :is_custom_game, type: Boolean
+            optional :is_installed, type: Boolean
+            optional :is_installing, type: Boolean
+            optional :is_launching, type: Boolean
+            optional :is_running, type: Boolean
+            optional :is_uninstalling, type: Boolean
+            optional :last_activity, type: DateTime
+            optional :manual, type: String
+            optional :modified, type: DateTime
+            optional :notes, type: String
+            optional :play_count, type: Integer
+            optional :playtime, type: Integer
+            optional :plugin_id, type: String
+            optional :post_script, type: String
+            optional :pre_script, type: String
+            optional :release_date, type: Date
+            optional :sorting_name, type: String
+            optional :use_global_game_started_script, type: Boolean
+            optional :use_global_post_script, type: Boolean
+            optional :use_global_pre_script, type: Boolean
+            optional :user_score, type: Integer
+            optional :version, type: String
           end
         end
         post "" do
