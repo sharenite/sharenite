3c54b2118e495f614dc232ae6662f7a9be7833d4
Dawid Karczewski <dawidxen@gmail.com>
2022-08-30 01:24:38 +0200

    Adds SSL ignore for Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,8 +2,10 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://sharenite-redis:6379/0' } if Rails.env.development?
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } } # https://stackoverflow.com/questions/65834575/how-to-enable-tls-for-redis-6-on-sidekiq
 end
   
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://sharenite-redis:6379/0' } if Rails.env.development?
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } } # https://stackoverflow.com/questions/65834575/how-to-enable-tls-for-redis-6-on-sidekiq
 end
