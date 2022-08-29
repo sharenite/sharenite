814f63d9b10f8a0f2aaf56ccc0a2ba95def7d94b
Dawid Karczewski <dawidxen@gmail.com>
2022-08-30 01:29:16 +0200

    Fixes sidekiq config for dev

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://sharenite-redis:6379/0' } if Rails.env.development?
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } } # https://stackoverflow.com/questions/65834575/how-to-enable-tls-for-redis-6-on-sidekiq
+  config.redis = if Rails.env.development?
+    { url: 'redis://sharenite-redis:6379/0' } 
+  else
+    { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } } # https://stackoverflow.com/questions/65834575/how-to-enable-tls-for-redis-6-on-sidekiq
+                 end
 end
   
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://sharenite-redis:6379/0' } if Rails.env.development?
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } } # https://stackoverflow.com/questions/65834575/how-to-enable-tls-for-redis-6-on-sidekiq
+  config.redis = if Rails.env.development?
+    { url: 'redis://sharenite-redis:6379/0' } 
+  else
+    { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } } # https://stackoverflow.com/questions/65834575/how-to-enable-tls-for-redis-6-on-sidekiq
+                 end
 end
