9546dfc9b41ba336091bd34878127ca684df8f3c
Dawid Karczewski <dawidxen@gmail.com>
2022-08-29 15:41:31 +0200

    Adds another attempt at optimizing full library resync

--- app/controllers/api/v1/games.rb
+++ app/controllers/api/v1/games.rb
@@ -51,9 +51,45 @@ module API
         end
         post "" do
           current_user.games.delete_all
-          params[:games].each do |game|
-            Game.create!(game.merge(user: current_user))
+          params[:games].map do |game|
+            game['user_id'] = current_user.id
+            game['added'] = nil unless game.key?('added')
+            game['community_score'] = nil unless game.key?('community_score')
+            game['critic_score'] = nil unless game.key?('critic_score')
+            game['description'] = nil unless game.key?('description')
+            game['favorite'] = nil unless game.key?('favorite')
+            game['game_id'] = nil unless game.key?('game_id')
+            game['game_started_script'] = nil unless game.key?('game_started_script')
+            game['hidden'] = nil unless game.key?('hidden')
+            game['include_library_plugin_action'] = nil unless game.key?('include_library_plugin_action')
+            game['install_directory'] = nil unless game.key?('install_directory')
+            game['is_custom_game'] = nil unless game.key?('is_custom_game')
+            game['is_installed'] = nil unless game.key?('is_installed')
+            game['is_installing'] = nil unless game.key?('is_installing')
+            game['is_launching'] = nil unless game.key?('is_launching')
+            game['is_running'] = nil unless game.key?('is_running')
+            game['is_uninstalling'] = nil unless game.key?('is_uninstalling')
+            game['last_activity'] = nil unless game.key?('last_activity')
+            game['manual'] = nil unless game.key?('manual')
+            game['modified'] = nil unless game.key?('modified')
+            game['notes'] = nil unless game.key?('notes')
+            game['play_count'] = nil unless game.key?('play_count')
+            game['playtime'] = nil unless game.key?('playtime')
+            game['plugin_id'] = nil unless game.key?('plugin_id')
+            game['post_script'] = nil unless game.key?('post_script')
+            game['pre_script'] = nil unless game.key?('pre_script')
+            game['release_date'] = nil unless game.key?('release_date')
+            game['sorting_name'] = nil unless game.key?('sorting_name')
+            game['use_global_game_started_script'] = nil unless game.key?('use_global_game_started_script')
+            game['use_global_post_script'] = nil unless game.key?('use_global_post_script')
+            game['use_global_pre_script'] = nil unless game.key?('use_global_pre_script')
+            game['user_score'] = nil unless game.key?('user_score')
+            game['version'] = nil unless game.key?('version')
+            game
           end
+          # rubocop:disable Rails/SkipsModelValidations
+          Game.insert_all(params[:games])
+          # rubocop:enable all
           current_user.games.reload
         end
       end
