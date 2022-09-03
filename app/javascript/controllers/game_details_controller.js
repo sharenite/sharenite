426b3ddeddb2084bf1697c1031870208f29b3b6d
Dawid Karczewski <dawidxen@gmail.com>
2022-09-03 15:16:11 +0200

    Cleans up experience on mobile

--- app/javascript/controllers/game_details_controller.js
+++ app/javascript/controllers/game_details_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     document
-      .querySelectorAll("#game-details__description p img")
+      .querySelectorAll(".game-details__description p img")
       .forEach((el) => el.setAttribute("class", "img-fluid"))
   }
 }
