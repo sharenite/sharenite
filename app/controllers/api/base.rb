6e85ee27134f3e7c2c928550a68758feb144a0ff
Dawid Karczewski <dawidxen@gmail.com>
2022-09-03 15:22:02 +0200

    Removes Rollbar

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -7,11 +7,6 @@ module API
   class Base < Grape::API
     before { authenticate_user! }
 
-    rescue_from :all do |e|
-      raise e if Rails.env.development?
-      Rollbar.error(e)
-      error_response(message: "Internal server error", status: 500)
-    end
     use Appsignal::Grape::Middleware
     mount API::V1::Base
   end
