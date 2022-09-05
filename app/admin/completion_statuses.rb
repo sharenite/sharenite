428fe257bbfacdcbf657b54863132edde1484687
Dawid Karczewski <dawidxen@gmail.com>
2022-09-05 21:06:41 +0200

    Adds game parameters to admin panel

---
+++ app/admin/completion_statuses.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register CompletionStatus do
+  config.sort_order = "created_at_desc"
+  menu parent: "Games", priority: 2
+
+  belongs_to :user, optional: true
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  index do
+    id_column
+    column :user
+    column :name
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :user_email, as: :string
+end
