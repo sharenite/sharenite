fe096235a78494b54d25c46a13a8824f41277ebf
Xenor <xenor@sharenite.link>
2023-01-30 18:24:58 +0100

    Improves dead queue processing

--- app/consumers/dead_messages_consumer.rb
+++ app/consumers/dead_messages_consumer.rb
@@ -10,7 +10,11 @@ class DeadMessagesConsumer < ApplicationConsumer
     messages.each do |message|
       variables(message.payload)
       finished_processing_at = Time.current
-      @sync_job.update(finished_processing_at:, processing_time: finished_processing_at - @sync_job.started_processing_at)
+      if @sync_job.started_processing_at.nil?
+        @sync_job.update(started_processing_at: finished_processing_at, finished_processing_at:, waiting_time: finished_processing_at - @sync_job.created_at, processing_time: 0)
+      else
+        @sync_job.update(finished_processing_at:, processing_time: finished_processing_at - @sync_job.started_processing_at)
+      end
       @sync_job.status_dead!
       # rubocop:disable Style/GlobalVars
       $redis.expire("syncjob:#{@sync_job.id}", 2_678_400)
