5565402adcc16beb2b5730a69690ac088ca71ce4
Xenor <xenor@sharenite.link>
2023-01-31 10:25:08 +0100

    Restores max poll interval for consumer

--- karafka.rb
+++ karafka.rb
@@ -5,6 +5,7 @@ class KarafkaApp < Karafka::App
   # rubocop:disable Metrics/BlockLength
   setup do |config|
     config.kafka = { "bootstrap.servers": "sharenite-kafka:9092" }
+    config.kafka[:"max.poll.interval.ms"] = 3_600_000
     config.client_id = "sharenite_app"
     # Recreate consumers with each batch. This will allow Rails code reload to work in the
     # development mode. Otherwise Karafka process would not be aware of code changes
