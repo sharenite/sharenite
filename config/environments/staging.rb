93a85df0efa002cd69c068d0031b861845c9f839
Xenor <xenor@sharenite.link>
2026-02-28 15:24:11 +0100

    Fix staging healthcheck behind kamal-proxy SSL termination

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,5 +4,8 @@
 require File.expand_path("production.rb", __dir__)
 
 Rails.application.configure do
-  # Here override any defaults
+  # Trust SSL termination at kamal-proxy and avoid redirecting the health check.
+  config.assume_ssl = true
+  config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+  config.silence_healthcheck_path = "/up"
 end
